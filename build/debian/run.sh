cb50a55b3adb562eaf498946168ed1fa6ce88d47
Aoang <aoang@x2oe.com>
2022-11-22 14:00:51 +0800

    Add kubectl yml

--- build/debian/run.sh
+++ build/debian/run.sh
@@ -2,10 +2,8 @@
 
 /etc/init.d/ssh restart
 
-SRV="127.0.0.1:8080"
-
 run(){
-  RESULT=$(curl -s "http://$SRV/api/v1/nsa/debian/$(arch | sed s/aarch64/arm64/ | sed s/x86_64/amd64/)/register")
+  RESULT=$(curl -s "http://$SERVICE/api/v1/nsa/debian/$(arch | sed s/aarch64/arm64/ | sed s/x86_64/amd64/)/register")
 
   if [ "$RESULT" -eq "" ] ; then
     echo "again"
